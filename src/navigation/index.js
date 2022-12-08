dc5dd6c4f4747a6cdb3d530e227228be6b958992
denfushi <awadomar@gmail.com>
2022-12-07 20:05:20 -0800

    renamed dashboard > home and home > profile

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SignInScreen from "../screens/SignInScreen";
@@ -6,6 +6,7 @@ import SignUpScreen from "../screens/SignUpScreen";
 import ConfirmEmailScreen from "../screens/ConfirmEmailScreen";
 import ForgotPasswordScreen from "../screens/ForgotPasswordScreen";
 import NewPasswordScreen from "../screens/NewPasswordScreen";
+import ProfileScreen from "../screens/ProfileScreen";
 import HomeScreen from "../screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
@@ -25,7 +26,8 @@ const Navigation = () => {
         <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />
         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
         <Stack.Screen name="NewPassword" component={NewPasswordScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={ProfileScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,11 @@
+import { View } from "react";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <h1>HI</h1>
+    </View>
+  );
+};
+
+export default HomeScreen;
