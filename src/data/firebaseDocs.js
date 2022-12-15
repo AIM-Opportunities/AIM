f316dd5b810c393db87ac246e65c1e70e519d0d4
denfushi <awadomar@gmail.com>
2022-12-14 22:30:41 -0800

    pull data correctly for firebase

--- src/data/firebaseDocs.js
+++ src/data/firebaseDocs.js
@@ -9,11 +9,12 @@ const data = () => {
     getDocs(collection(db, "opportunities")).then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
         // Push each document into the docs array and set the state with the updated array
-        setDocs((docs) => [...docs, doc.data()]);
+        setDocs([...docs, doc.data()]);
       });
     });
   }, []);
 
+  // Return the docs state without resetting it
   return docs;
 };
 
