ce52d63477aaac1dd4850e092ba0fe3c7f449bd1
denfushi <awadomar@gmail.com>
2022-12-22 23:53:02 -0800

    fix for blue screen

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -63,7 +63,7 @@ const HomeScreen = () => {
       getDocs(collection(db, "opportunities")).then((querySnapshot) => {
         // Create a new array to store the filtered opportunities docs
         const newDocs = [...docs];
-        let count
+        let count;
         let hasMatches = false;
         querySnapshot.forEach((doc) => {
           // Check if the opportunity doc's "lookingFor" field matches any of the userProfile docs' "lookingFor" field
@@ -103,7 +103,7 @@ const HomeScreen = () => {
       getDocs(collection(db, "opportunities")).then((querySnapshot) => {
         // Create a new array to store the filtered opportunities docs
         const newDocs = [];
-        let count 
+        let count = 0;
         let hasMatches = false;
         querySnapshot.forEach((doc) => {
           // Check if the opportunity doc's "lookingFor" field matches any of the userProfile docs' "lookingFor" field
