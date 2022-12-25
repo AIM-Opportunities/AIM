f32a9e1110c4e712618484a018788bded85dcb06
denfushi <awadomar@gmail.com>
2022-12-25 13:11:34 -0800

    load all opps only once in a random order

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,25 +1,17 @@
 import React, { useState, useEffect, lazy, Suspense } from "react";
-import {
-  View,
-  Text,
-  FlatList,
-  StyleSheet,
-  Dimensions,
-  RefreshControl,
-} from "react-native";
-import { includes } from "lodash";
+import { View, Text, FlatList, StyleSheet, Dimensions } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { includes } from "lodash";
 import CustomButton from "../../components/CustomButton";
 import useSwipe from "../../components/UseSwipe";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../../firebase/firebase-config";
-import { ScrollView } from "react-native-gesture-handler";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   const { onTouchStart, onTouchEnd } = useSwipe(onSwipeUp, onSwipeDown, 6);
   const [docs, setDocs] = useState([]);
-  const [hasMatches, setNoMatches] = useState(false);
+  const [allDocIds, setAllDocIds] = useState([]); // new state to track all unique doc ids
 
   useEffect(() => {
     // Get the userProfile docs
@@ -35,21 +27,18 @@ const HomeScreen = () => {
         // Create a new array to store the filtered opportunities docs
 
         const newDocs = [];
-        let hasMatches = false;
         let count = 0;
         querySnapshot.forEach((doc) => {
-          // Push the opportunity doc into the newDocs array if it matches
           if (count < 3) {
             newDocs.push({ ...doc.data(), id: doc.id });
             count++;
-            hasMatches = true;
+            setAllDocIds((prevAllDocIds) => [...prevAllDocIds, doc.id]); // add the doc id to the allDocIds array
           }
         });
         // Randomize the array of docs
         newDocs.sort(() => Math.random() - 0.5);
         // Set the state with the newDocs array
         setDocs(newDocs);
-        setNoMatches(!hasMatches);
       });
     });
   }, []);
@@ -57,8 +46,8 @@ const HomeScreen = () => {
   //FUNCTIONS
   const onEndReached = () => {
     console.log("END");
-     // Get the userProfile docs
-     getDocs(collection(db, "userProfiles")).then((userProfileSnapshot) => {
+    // Get the userProfile docs
+    getDocs(collection(db, "userProfiles")).then((userProfileSnapshot) => {
       // Create a new array to store the userProfile docs
       const userProfiles = [];
       userProfileSnapshot.forEach((doc) => {
@@ -66,37 +55,65 @@ const HomeScreen = () => {
       });
       // Get the opportunities docs and filter them based on the userProfile docs
       getDocs(collection(db, "opportunities")).then((querySnapshot) => {
-
         // Create a new array to store the filtered opportunities docs
         const newDocs = [...docs];
         let count = 0;
-        let hasMatches = false;
         querySnapshot.forEach((doc) => {
-          // Push the opportunity doc (that is not already in the array) into the newDocs array if it matches
-          if (!newDocs.find((d) => d.id === doc.id)) {
+          if (
+            !newDocs.find((d) => d.id === doc.id) &&
+            !includes(allDocIds, doc.id)
+          ) {
             if (count < 3) {
               newDocs.push({ ...doc.data(), id: doc.id });
               count++;
-              hasMatches = true;
+              setAllDocIds((prevAllDocIds) => [...prevAllDocIds, doc.id]); // add the doc id to the allDocIds array
             }
           }
         });
-     // Randomize the array of docs
-     newDocs.sort(() => Math.random() - 0.5);
-     // Set the state with the newDocs array
-     setDocs(newDocs);
-     setNoMatches(!hasMatches);
-   });
- });
-   
-};
+        // Randomize the array of docs
+        newDocs.sort(() => Math.random() - 0.5);
+        // Set the state with the newDocs array
+        setDocs(newDocs);
+      });
+    });
+  };
   function onSwipeUp() {
     console.log("SWIPE_UP");
   }
 
   function onSwipeDown() {
     console.log("SWIPE_DOWN");
-    
+    // Get the userProfile docs
+    getDocs(collection(db, "userProfiles")).then((userProfileSnapshot) => {
+      // Create a new array to store the userProfile docs
+      const userProfiles = [];
+      userProfileSnapshot.forEach((doc) => {
+        userProfiles.push(doc.data());
+      });
+      // Get the opportunities docs and filter them based on the userProfile docs
+      getDocs(collection(db, "opportunities")).then((querySnapshot) => {
+        // Create a new array to store the filtered opportunities docs
+        const newDocs = [...docs];
+        let count = 0;
+        querySnapshot.forEach((doc) => {
+          // Push the opportunity doc (that is not already in the array) into the newDocs array if it matches
+          if (
+            !newDocs.find((d) => d.id === doc.id) &&
+            !includes(allDocIds, doc.id)
+          ) {
+            if (count < 3) {
+              newDocs.push({ ...doc.data(), id: doc.id });
+              count++;
+              setAllDocIds((prevAllDocIds) => [...prevAllDocIds, doc.id]); // add the doc id to the allDocIds array
+            }
+          }
+        });
+        // Randomize the array of docs
+        newDocs.sort(() => Math.random() - 0.5);
+        // Set the state with the newDocs array
+        setDocs(newDocs);
+      });
+    });
   }
   const buttonPress = () => {
     navigation.navigate("Profile");
